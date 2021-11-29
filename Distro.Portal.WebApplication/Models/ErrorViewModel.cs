aad9d0402531b44b65dce498a7f7290139092839
Marius Vorster <marius.vrstr@gmail.com>
2021-11-29 11:02:25 +0200

    Added basic MVC Web App scaffold project

---
+++ Distro.Portal.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Distro.Portal.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
