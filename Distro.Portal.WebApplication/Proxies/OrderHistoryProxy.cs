463b4a911ab6265757837840bc351ce6023d7810
Marius Vorster <marius.vrstr@gmail.com>
2021-12-02 23:52:35 +0200

    Added Order Proxies

---
+++ Distro.Portal.WebApplication/Proxies/OrderHistoryProxy.cs
@@ -0,0 +1,20 @@
+﻿using Distro.Ordering.Domain.Contracts;
+using Distro.Ordering.Domain.Entities;
+
+namespace Distro.Portal.WebApplication.Proxies
+{
+    public class OrderHistoryProxy : IOrderHistoryService
+    {
+        private readonly IOrderHistoryService _orderHistoryService;
+        
+        public OrderHistoryProxy(IOrderHistoryService orderHistoryService)
+        {
+            _orderHistoryService = orderHistoryService;
+        }
+
+        public IEnumerable<Order> GetOrderHistory(Guid customerId)
+        {
+            return _orderHistoryService.GetOrderHistory(customerId);
+        }
+    }
+}
---
+++ Distro.Portal.WebApplication/Proxies/OrderProxy.cs
@@ -0,0 +1,30 @@
+﻿using Distro.Ordering.Domain.Contracts;
+using Distro.Ordering.Domain.Entities;
+
+namespace Distro.Portal.WebApplication.Proxies
+{
+    public class OrderProxy : IOrderService
+    {
+        private readonly IOrderService _orderService;
+        
+        public OrderProxy(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public Order AddOrder(Order order)
+        {
+            return _orderService.AddOrder(order);
+        }
+
+        public Order? GetOrderById(Guid Id)
+        {
+            return _orderService.GetOrderById(Id);
+        }
+
+        public Order UpdateOrder(Order order)
+        {
+            return _orderService.UpdateOrder(order);
+        }
+    }
+}
